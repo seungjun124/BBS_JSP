8dbd033b5c66b0786047eae65847f5b947a70b13
seungjun124 <127902249+seungjun124@users.noreply.github.com>
2023-03-16 14:55:00 +0900

    Add files via upload

---
+++ src/user/User.java
@@ -0,0 +1,41 @@
+package user;
+
+public class User {
+	
+	private String USERID;
+	private String USERPASSWORD;
+	private String USERNAME;
+	private String USERGENDER;
+	private String USEREMAIL;
+	
+	public String getUSERID() {
+		return USERID;
+	}
+	public void setUSERID(String uSERID) {
+		USERID = uSERID;
+	}
+	public String getUSERPASSWORD() {
+		return USERPASSWORD;
+	}
+	public void setUSERPASSWORD(String uSERPASSWORD) {
+		USERPASSWORD = uSERPASSWORD;
+	}
+	public String getUSERNAME() {
+		return USERNAME;
+	}
+	public void setUSERNAME(String uSERNAME) {
+		USERNAME = uSERNAME;
+	}
+	public String getUSERGENDER() {
+		return USERGENDER;
+	}
+	public void setUSERGENDER(String uSERGENDER) {
+		USERGENDER = uSERGENDER;
+	}
+	public String getUSEREMAIL() {
+		return USEREMAIL;
+	}
+	public void setUSEREMAIL(String uSEREMAIL) {
+		USEREMAIL = uSEREMAIL;
+	}
+}
